9cc796fa72425687a8c4aeaa4159dbcc6bc8d200
Debam06 <das.debam@outlook.com>
2026-02-10 01:19:22 +0530

    Changed BlacklistedToken.js

--- models/BlacklistedToken.js
+++ models/BlacklistedToken.js
@@ -3,6 +3,7 @@ import sequelize from "../config/db.js";
 
 const BlacklistedToken = sequelize.define("BlacklistedToken", {
   token: { type: DataTypes.STRING, allowNull: false },
+  expiresAt: { type: DataTypes.DATE, allowNull: false }  // ✅ add this
 });
 
 export default BlacklistedToken;
